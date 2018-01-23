08adb174d44acbcca2b3abcf0f8cae9a0cba5968
Ivo Bathke <ivo.bathke@gmail.com>
2018-01-23 12:53:08 +0100

    fixes

--- docker/container/apache_php7/entrypoint.d/010-oxid.sh
+++ docker/container/apache_php7/entrypoint.d/010-oxid.sh
@@ -4,7 +4,7 @@ set -e
 # bootstrap oxid files/project
 if [ ! -f /var/www/html/source/config.inc.php ]
 then
-	composer create-project --keep-vcs oxid-esales/oxideshop-project . ${OXID_VERSION}
+	composer create-project --keep-vcs oxid-esales/oxideshop-project /var/www/html ${OXID_VERSION}
 	mv /var/www/config.inc.php /var/www/html/source/config.inc.php
 	chown -R www-data:www-data /var/www/html/source/
 	chmod 777 /var/www/html/source/log /var/www/html/source/tmp
